4475453ea75dde02e59b599680b6c38cf5bce480
ShaolunWang <wang.shaolun@gmail.com>
2021-09-23 20:36:39 +0000

    init project

---
+++ src/main/java/uk/ac/ed/inf/LongLat.java
@@ -0,0 +1,25 @@
+package uk.ac.ed.inf;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+public class LongLat()
+{
+    public double longitude;
+    public double latitude;
+    private int angle;
+    public LongLat(String longitude, String latitude)
+    {
+        this.longitude = longitude;
+        this.latitude  = latitude;
+        angle          = -1000;
+    }
+
+
+
+
+}
+
