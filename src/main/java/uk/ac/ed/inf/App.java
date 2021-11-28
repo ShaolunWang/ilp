430ea59762a6c659e7b6e92fa321f67431cdc282
ShaolunWang <wang.shaolun@gmail.com>
2021-11-28 17:02:22 +0000

    uwu

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
 
-public class Test
+public class App
 {
     public static void main(String[] args) throws ParseException, SQLException 
 	{
